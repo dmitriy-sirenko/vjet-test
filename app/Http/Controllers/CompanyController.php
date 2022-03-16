cadb2519aefe51e9824d748058b7105b006237cd
Dmitriy Sirenko <dmitriy.sirenko@gmail.com>
2022-03-17 00:18:03 +0200

    refactor

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CompanyCollection;
-use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
